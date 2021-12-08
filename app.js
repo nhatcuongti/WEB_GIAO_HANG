e7fa84a9706c335c68ed67bc90f2b4b57bde1e75
lhnhat <nhatks147@gmail.com>
2021-12-09 00:02:35 +0700

    layout

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from'express';
+import { engine } from 'express-handlebars';
+import numeral from 'numeral';
+
+
 const app = express()
 const port = 3000
 
+app.engine('hbs', engine({
+    defaultLayout: 'main.hbs',
+    helpers: {
+        format_number(val) {
+            return numeral(val).format('0, 0');
+        }
+    }
+}));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
-app.listen(port, () => {
+app.get('/', function (req, res){
+    res.render('test');
+});
+
+app.listen(port, function ()  {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
